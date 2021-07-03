fd687a2a9b0f3d72dce1c024deba742abeb46ec4
DESKTOP-JBQCEBD\motro <motrocgabi@gmail.com>
2021-07-03 16:06:36 +0300

    framework entity try

--- UnblockMe/UnblockMe/Controllers/CarController.cs
+++ UnblockMe/UnblockMe/Controllers/CarController.cs
@@ -3,11 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnblockMe.Models;
 
 namespace UnblockMe.Controllers
 {
     public class CarController : Controller
     {
+        private readonly UnblockMeContext _context;
+        public CarController(UnblockMeContext unlockMeContext)
+        {
+            _context = unlockMeContext;
+        }
+
+        public IActionResult ViewLicencePlate()
+        {
+            var cars = _context.Car.Select(car => car).ToList();
+            return View(cars);
+        }
+
         public IActionResult AddLicencePlate()
         {
             return View();
