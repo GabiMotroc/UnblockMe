3fb68dafe522fa6c46ea97ac73f0ea09ff79489e
DESKTOP-EFE8QPM\Gabi <motrocgabi@gmail.com>
2021-07-05 01:48:18 +0300

    search implemented

--- UnblockMe/UnblockMe/Controllers/CarController.cs
+++ UnblockMe/UnblockMe/Controllers/CarController.cs
@@ -20,12 +20,18 @@ namespace UnblockMe.Controllers
             _logger = logger;
             _context = unlockMeContext;
         }
-        
+
         public IActionResult ViewLicencePlate()
         {
             var cars = _context.Car.Select(a => a).ToList();
             return View(cars);
         }
+
+        //public IActionResult ViewLicencePlate(string value)
+        //{
+        //    var cars = _context.Car.Select(a => a.LicencePlate == value);
+        //    return View(cars);
+        //}
 
         //public IActionResult AddCar()
         //{
@@ -34,7 +40,7 @@ namespace UnblockMe.Controllers
 
         public IActionResult AddCar(Car item)
         {
-            try 
+            try
             {
                 if (!item.Equals(null))
                 {
@@ -46,11 +52,48 @@ namespace UnblockMe.Controllers
             }
             catch (Exception e)
             {
-                 //nu e chiar nevoie
+                //nu e chiar nevoie
             }
-            return View();  
+            return View();
         }
         
+        [HttpPost]
+        public IActionResult RemoveCar(string value)
+        {
+            try 
+            { 
+            var car = _context.Car.Find(value);
+            this._context.Car.Remove(car);
+            this._context.SaveChanges();
+            }
+            
+            catch
+            {
+
+            }
+            //return View("ViewLicencePlate.cshtml");
+            this.ViewLicencePlate();
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult SearchCar(Car car)
+        {
+
+            var text = car.LicencePlate;
+            if(text != null)
+            {
+                var Model = _context.Car.Where(a => a.LicencePlate.Contains(text)).ToList();
+                try
+                {
+                    if (Model.Equals(null))
+                        return View(ViewName);
+                }
+                catch { }
+                return View("ViewLicencePlate", Model);
+            }
+            return View(ViewName);
+        }
 
         //public ActionResult AddLicencePlate()
         //{
--- UnblockMe/UnblockMe/Models/Car.cs
+++ UnblockMe/UnblockMe/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,12 +10,38 @@ namespace UnblockMe.Models
 {
     public partial class Car
     {
-        public string LicencePlate { get; set; }
+        private string _licencePlate;
+        private string _blockedBy;
+        private string _blocks;
+        public string LicencePlate 
+        {
+            get => _licencePlate;
+            set
+            {
+                this._licencePlate = value.Replace(" ", "").ToLower(); 
+            }  
+        }
         public string Maker { get; set; }
         public string Model { get; set; }
         public string Colour { get; set; }
-        public string BockedBy { get; set; }
-        public string Blocks { get; set; }
+        public string BockedBy 
+        {
+            get => _blockedBy;
+            set
+            {
+                if(value != null)
+                    this._blockedBy = value.Replace(" ", "").ToLower();
+            }
+        }
+        public string Blocks 
+        {
+            get => _blocks;
+            set
+            {
+                if (value != null)  
+                    this._blocks = value.Replace(" ", "").ToLower();
+            }
+        }
         public int OwnerId { get; set; }
 
         public virtual User Owner { get; set; }
