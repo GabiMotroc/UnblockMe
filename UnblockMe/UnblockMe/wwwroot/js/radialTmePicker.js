ee56c0d99570d1b6d1f60e6424beb4c865402b7c
DESKTOP-EFE8QPM\Gabi <motrocgabi@gmail.com>
2021-08-21 01:05:09 +0300

    rotation part fully working

--- UnblockMe/UnblockMe/wwwroot/js/radialTmePicker.js
+++ UnblockMe/UnblockMe/wwwroot/js/radialTmePicker.js
@@ -1,83 +1,86 @@
-﻿class radialTimePicker {
+﻿class Wheel {
     constructor() {
-        this.knobElm = (document.getElementById("knob"));
-
-        this.knobElm.addEventListener("mousedown", e => {
+        this.wheelElm = document.getElementById('wheel');
+        this.iconElm = document.getElementById('icon');
+        this.wheelElm.addEventListener('mousedown', e => {
             this.onGrab(e.clientX, e.clientY);
         });
-
-        this.knobElm.addEventListener("mousemove", e => {
+        window.addEventListener('mousemove', e => {
             if (e.which == 1)
                 this.onMove(e.clientX, e.clientY);
             else if (!this.isDragging)
                 this.onRelease()
-        });
 
+        });
         window.addEventListener('mouseup', this.onRelease.bind(this));
 
-        /*this.knobElm.addEventListener("mouseup", e => {
-            this.onRelease(e);
-        });*/
+        this.wheelElm.addEventListener('touchstart', e => {
+            this.onGrab(e.touches[0].clientX, e.touches[0].clientY);
+        });
+        window.addEventListener('touchmove', e => {
+            this.onMove(e.touches[0].clientX, e.touches[0].clientY);
+        });
+        window.addEventListener('touchend', this.onRelease.bind(this));
 
         this.calculatePositions();
         window.addEventListener('resize', this.calculatePositions.bind(this));
 
         this.currentAngle = 0;
-        this.lastAngle = 0;
-        this.lastAngles = [0, 0, 0];
-        this.start = {
-            x: null,
-            y: null,
-        };
+        this.oldAngle = 0;
+        this.isDragging = false;
+        this.startX = null;
+        this.startY = null;
 
-        this.positionCallbacks = [];
     }
 
     calculatePositions() {
-        this.wheelWidth = this.knobElm.getBoundingClientRect()['width'];
-        this.wheelHeight = this.knobElm.getBoundingClientRect()['height']
-        this.wheelX = this.knobElm.getBoundingClientRect()['x'] + this.wheelWidth / 2;
-        this.wheelY = this.knobElm.getBoundingClientRect()['y'] + this.wheelHeight;
-    }
-
-    onPositionChange(callback) {
-        this.positionCallbacks.push(callback);
-    }
-
-    onRelease() {
-
-        this.oldAngle = this.currentAngle;
-
+        this.wheelWidth = this.wheelElm.getBoundingClientRect()['width'];
+        this.wheelHeight = this.wheelElm.getBoundingClientRect()['height']
+        this.wheelX = this.wheelElm.getBoundingClientRect()['x'] + this.wheelWidth / 2;
+        this.wheelY = this.wheelElm.getBoundingClientRect()['y'] + this.wheelHeight;
     }
 
     onGrab(x, y) {
-        this.startAngle = this.calculateAngleDegrees(x, y);
+        this.isDragging = true;
+        this.startAngle = this.calculateAngle(x, y);
     }
 
     onMove(x, y) {
+        if (!this.isDragging)
+            return
 
-        this.currentAngle = this.calculateAngleDegrees(x, y);
-
-        this.lastAngles.shift();
-        this.lastAngles.push(this.currentAngle);
-
-        let deltaAngle = this.calculateAngleDegrees(x, y) - this.startAngle;
+        let deltaAngle = this.calculateAngle(x, y) - this.startAngle;
         this.currentAngle = deltaAngle + this.oldAngle;
 
         this.render(this.currentAngle);
     }
 
-    render(deg) {
-        this.knobElm.style.transform = "rotate(" + deg + "deg)";
-    }
-
-    calculateAngleDegrees(x, y) {
-        let xLength = x - this.wheelX;
-        let yLength = y - this.wheelY;
+    calculateAngle(currentX, currentY) {
+        let xLength = currentX - this.wheelX;
+        let yLength = currentY - this.wheelY;
         let angle = Math.atan2(xLength, yLength) * (180 / Math.PI);
+
+        console.log(360 - angle);
+
+        //if (360 - angle < 240)
+        //    return 240;
+        //if (360 - angle > 480)
+        //    return 480;
+
         return 365 - angle;
     }
 
-}
-let wheel = new radialTimePicker();
+    onRelease() {
+        if (this.isDragging) {
+            this.isDragging = false;
+            this.oldAngle = this.currentAngle;
+        }
+    }
 
+    render(deg) {
+        this.wheelElm.style.transform = `rotate(${deg}deg)`;
+        this.iconElm.style.transform = `rotate(${360 - deg}deg`;
+    }
+}
+
+let wheel = new Wheel();
