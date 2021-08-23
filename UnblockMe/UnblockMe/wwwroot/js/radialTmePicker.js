5adbf7a254ff6822e1e19bfdab7feb7782623d53
DESKTOP-EFE8QPM\Gabi <motrocgabi@gmail.com>
2021-08-23 19:23:27 +0300

    working boundaries

--- UnblockMe/UnblockMe/wwwroot/js/radialTmePicker.js
+++ UnblockMe/UnblockMe/wwwroot/js/radialTmePicker.js
@@ -2,6 +2,8 @@
     constructor() {
         this.wheelElm = document.getElementById('wheel');
         this.iconElm = document.getElementById('icon');
+        this.displayElm = document.getElementById('display');
+
         this.wheelElm.addEventListener('mousedown', e => {
             this.onGrab(e.clientX, e.clientY);
         });
@@ -27,6 +29,7 @@
 
         this.currentAngle = 0;
         this.oldAngle = 0;
+        this.lastAngle = 0;
         this.isDragging = false;
         this.startX = null;
         this.startY = null;
@@ -42,45 +45,62 @@
 
     onGrab(x, y) {
         this.isDragging = true;
-        this.startAngle = this.calculateAngle(x, y);
+
+        var matches = this.wheelElm.style.transform.match(/(\d+)/);
+        if (matches) {
+            this.startAngle = 360 - matches[0] % 360;
+            //console.log(360 - matches[0] % 360);
+        }
+        else {
+            this.startAngle = this.calculateAngle(x, y);
+        }
+        //this.startAngle = parseInt (this.wheelElm.style.transform);
+        //this.calculateAngle(x, y);
     }
 
     onMove(x, y) {
         if (!this.isDragging)
             return
 
-        let deltaAngle = this.calculateAngle(x, y) - this.startAngle;
-        this.currentAngle = deltaAngle + this.oldAngle;
+        let deltaAngle = this.calculateAngle(x, y) - 180;
+
+
+        this.currentAngle = deltaAngle;
+
+        if (this.lastAngle > 330 && this.currentAngle < 180)
+            this.currentAngle = 359.99;
+
+        if (this.lastAngle < 30 && this.currentAngle > 180)
+            this.currentAngle = 0.01;
+
+        console.log(deltaAngle);
 
         this.render(this.currentAngle);
+
+        this.lastAngle = this.currentAngle;
     }
-
-    calculateAngle(currentX, currentY) {
-        let xLength = currentX - this.wheelX;
-        let yLength = currentY - this.wheelY;
-        let angle = Math.atan2(xLength, yLength) * (180 / Math.PI);
-
-        console.log(360 - angle);
-
-        //if (360 - angle < 240)
-        //    return 240;
-        //if (360 - angle > 480)
-        //    return 480;
-
-        return 365 - angle;
-    }
-
     onRelease() {
         if (this.isDragging) {
             this.isDragging = false;
             this.oldAngle = this.currentAngle;
         }
     }
+    calculateAngle(currentX, currentY) {
+        let xLength = currentX - this.wheelX;
+        let yLength = currentY - this.wheelY;
+        let angle = Math.atan2(xLength, yLength) * (180 / Math.PI);
+
+        return 360 - angle;
+    }
+
+
 
     render(deg) {
-        this.wheelElm.style.transform = `rotate(${deg}deg)`;
+        this.wheelElm.style.transform = `rotate(${deg % 360}deg)`;
         this.iconElm.style.transform = `rotate(${360 - deg}deg`;
+        this.displayElm
     }
+
 }
 
 let wheel = new Wheel();
